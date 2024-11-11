fea2ffc112a675985632afd558f15a37a0399c80
Jose Eduardo del Real Rico <eduardo_delreal98@gmail.com>
2024-11-11 17:40:19 -0600

    se creo un nuevo proyecto carro de compras

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// en react cuando estamos trabajando con listas que vamos a imprimir se tiene que indicar
+// un valor unico para identificar la fila que queremos renderizar 
+import Button from './Button.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const arr = [
+  'Jose Eduardo',
+  'Mariana',
+  'Juan Carlos',
+]
+
+const App = () => {
+  const miVariable = false
+
+  if(miVariable)
+  {
+    return <p>Mi variable dieo true!</p>
+  }
+  return(
+    <div>
+      <h1>Hola a todos!</h1>
+
+      /*el metodo map me va a regresar un nuevo arreglo */
+      {arr.map(el => <p>{el}</p>)}
+      <Button onClick={(e) => console.log('clickead',e)}>
+        Enviar
+      </Button>
     </div>
-  );
-}
+  )
+} 
 
-export default App;
+export default App
--- mi-app/src/index.js
+++ mi-app/src/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//  const root = ReactDOM.createRoot(document.getElementById('root'));
-//  root.render(
-//    <React.StrictMode>
-//      <App />
-//    </React.StrictMode>
-//   );
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+   );
 
-const Li = ({ children }) => <li>{children}</li>
-const X = () =>
-  <ul>
-    <Li>Lalo</Li>
-    <li>Alex</li>
-    <li>Ericka</li>
-  </ul>
+// En el ejemplo que se muestra a continuacion se esta usando propiedades como:
+// propiedad 'children' y la porpiedades custom que en el caso de este ejemplo es: 'estado'
+// const Li = ({children,sts, casa, edad }) =>
+//   <li>{children} se encuentra {sts}</li>
+
+// const X = () =>
+//   <ul>
+//     <Li 
+//       children = {'children'}
+//       sts = {'feliz'}
+//       casa = {false}
+//       edad = {24}
+//     >
+//       Lalo
+//     </Li>
+
+//     <Li
+//       sts = {'triste'}
+//     >Alex
+//     </Li>
+
+//     <Li
+//       sts = {'emocionada'}>Mariana</Li>
+//   </ul>
+
 //ReactDOM.render: se encarga de renderizar nuestras aplicaciones en React, puede recibir
 //un string o jsx
 //jsx: es azucar sintaxyca para no tener que utilizar "React.createElement()"
-ReactDOM.render(
-  <X/>, document.getElementById('root')
-)
+// ReactDOM.render(
+//   <X/>, document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
